cd005c6196cba84aa0602fe38297bd967e83e370
Luis Capelo <luiscape@gmail.com>
2015-08-04 14:15:23 -0400

    adding env variables and changed path of roeutes

--- config/prod.js
+++ config/prod.js
@@ -2,7 +2,7 @@ module.exports = {
   'CkanInstance': 'https://data.hdx.rwlabs.org/',
   'DataFolder': './data/',
   'InferDataTypes': false,
-  'ApiKey': 'XXX',
+  'ApiKey': process.env.DEFAULT_API_KEY,
   'version': 'v.0.1.0',
   'repository': 'https://github.com/luiscape/hdx-monitor-datastore'
 }
