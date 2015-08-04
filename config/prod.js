e25834bd1398dd4533c9b8c4cb5482794eeb0658
Luis Capelo <luiscape@gmail.com>
2015-08-04 13:24:34 -0400

    adding config files

---
+++ config/prod.js
@@ -0,0 +1,8 @@
+module.exports = {
+  'CkanInstance': 'https://data.hdx.rwlabs.org/',
+  'DataFolder': './data/',
+  'InferDataTypes': false,
+  'ApiKey': 'XXX',
+  'version': 'v.0.1.0',
+  'repository': 'https://github.com/luiscape/hdx-monitor-datastore'
+}
