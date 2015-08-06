a3e16613258a6a3f5009408921010fc7064b7de0
Luis Capelo <luiscape@gmail.com>
2015-08-06 15:23:04 -0400

    adding cors

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var express = require('express')
+var cors = require('cors')
 var app = express()
 var port = process.env.PORT || 5000
 
 //
 // Load and launch application.
 //
+app.use(cors())
 require('./app/datastore.js')(app)
 app.listen(port)
 console.log('DataStore service running on port: ' + port)
