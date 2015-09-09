2345546fd8f846a47ca6c15f8ac3391dfce5af6e
Luis Capelo <luiscape@gmail.com>
2015-09-09 13:34:43 -0400

    adding new dependencies for parsing data from requests

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 var express = require('express')
 var cors = require('cors')
 var app = express()
-var port = process.env.PORT || 5000
+var bodyParser = require('body-parser')
 
 //
 // App variables.
 //
 var _version = 'v.0.1.4'
+var port = process.env.PORT || 5000
 
 //
 // Load and launch application.
 //
 app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 require('./app/routes.js')(app)
 app.listen(port)
 console.log('DataStore service running on port: ' + port)
